cd0639679e1157cb6493e4f49297056d41f395d3
Dave Cassel <dmcassel@gmail.com>
2017-07-27 15:52:31 -0400

    removing unneccessary comment

--- build-addin.bat
+++ build-addin.bat
@@ -1,4 +1,3 @@
-rem cd C:\cygwin\home\paven\MLOS\Office2007\Addins\Word\Microsoft\MarkLogic_WordAddin\MarkLogic_WordAddin
 cd Addins\Word\Microsoft\MarkLogic_WordAddin\MarkLogic_WordAddin
 
 rem call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
